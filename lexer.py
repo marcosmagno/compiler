765c144e71db2c974db7585598d2afee8793bb78
Marcos Carvalho <marcosf40@hotmail.com>
2018-04-12 15:22:44 -0300

    commit

--- lexer.py
+++ lexer.py
@@ -1,7 +1,7 @@
 import time
 import sys
 from symbol_table import TabelaSimbolo
-from token import Token 
+from token import Token
 from tag import Tag_type
 from random import *
 import string
@@ -21,7 +21,6 @@ class Lexer(object):
 		self.char = ''
 
 
-
 		# open the file
 		try:
 			self.__file = open(input_file,'r')
@@ -32,13 +31,13 @@ class Lexer(object):
 		"""
 			this method handles the pointer in the file
 			seek() : Starts reading from the parameter that is passed
-			tell() : Get an integer representing the position of the pointer 
+			tell() : Get an integer representing the position of the pointer
 			tell() - 1: returns a position on the pointer"""
 
 		try:
 			self.__file.seek(self.__file.tell() - 1)
 		except Exception as e:
-			raise e		
+			raise e
 
 	def panic_mode(self, row, column, type_erro):
 		"""
@@ -48,7 +47,7 @@ class Lexer(object):
 		self.row_ = row
 		self.column_ = column
 		self.type_erro = type_erro
-		
+
 		self.command = ''
 
 		self.command = ''.join(map(str, str(self.type_erro) + " Linha: " + str(self.row_) + " Coluna: " + str(self.column_)))
@@ -58,12 +57,12 @@ class Lexer(object):
 		for i in self.erros:
 			print i
 
-		
+
 
 
 	def close_file(self):
 		self.__file.close()
-		
+
 
 	def nex_token(self):
 		""" This method handles the input file
@@ -73,24 +72,24 @@ class Lexer(object):
 
 		self.__list_lexema = []
 		while True:
-			
-			self.column = self.column + 1 
+
+			self.column = self.column + 1
 
 			# Read File
 			try:
-				self.c = self.__file.read(1)				
+				self.c = self.__file.read(1)
 				if not self.c:
 					self.EOF = -1
-					#return Token(Tag_type.EOF, "EOF", self.row, self.column)				
+					#return Token(Tag_type.EOF, "EOF", self.row, self.column)
 
 			except IOError as e:
-				raise e		
+				raise e
 
 			if self.__state == 1: # CASE 1
 
-				if not self.c:			
-					return Token(Tag_type.EOF, "EOF", self.row, self.column)				
-				
+				if not self.c:
+					return Token(Tag_type.EOF, "EOF", self.row, self.column)
+
 				elif self.c == "\n": # state 1
 					self.row = self.row + 1 # increase the line
 					self.column = 1 # returns the column count
@@ -100,7 +99,7 @@ class Lexer(object):
 				elif self.c == '\t': # state 1
 					self.column = self.column + 3
 
-				elif self.c == " ":	 # state 1				 
+				elif self.c == " ":	 # state 1
 					#self.column = self.column + 1
 					pass
 				elif self.c == "<":
@@ -111,11 +110,11 @@ class Lexer(object):
 
 				elif self.c == ">":
 					self.__state = 8 # state 8 return of the state 9 or 10
- 
+
 				elif self.c == "!":
 					self.__state = 11 # state 11 return of the 11
 
-				elif self.c == "-": 
+				elif self.c == "-":
 					self.__state = 1 # state 13
 					return Token(Tag_type.OP_MIN, "-", self.row, self.column) # return of the state 13
 
@@ -126,11 +125,11 @@ class Lexer(object):
 				elif self.c == "{":
 					self.__state = 1 # state 25
 					return Token(Tag_type.SMB_OBC, "{", self.row, self.column) # return of the state 25
-				
+
 				elif self.c == "}":
 					self.__state = 1 # state 26
 					return Token(Tag_type.SMB_CBC, "}", self.row, self.column) # return of the state 26
-				
+
 				elif self.c == "(":
 					self.__state = 1 # state 27
 					return Token(Tag_type.SMB_OPA, "(", self.row, self.column)	# return of the state 27
@@ -138,18 +137,18 @@ class Lexer(object):
 				elif self.c == ")":
 					self.__state = 1 # state 28
 					return Token(Tag_type.SMB_CPA, ")", self.row, self.column) # return of the state 28
-				
+
 				elif self.c == ",":
 					self.__state = 1 # state 29
-					return Token(Tag_type.SMB_COM, ",", self.row, self.column) # return of the state 29				
+					return Token(Tag_type.SMB_COM, ",", self.row, self.column) # return of the state 29
 
 				elif self.c == ";":
 					self.__state = 1 # state 30
-					return Token(Tag_type.SMB_SEM, ";", self.row, self.column) # return of the state 30			
-			
+					return Token(Tag_type.SMB_SEM, ";", self.row, self.column) # return of the state 30
+
 				elif self.c == "/":
 					self.__state = 16
-				
+
 				elif self.c == "*":
 					self.__state = 15
 
@@ -190,27 +189,27 @@ class Lexer(object):
 					self.file_pointer()
 					self.__state = 1
 					return Token(Tag_type.OP_MUL, "*", self.row, self.column)
-			
+
 			elif self.__state == 5: # CASE 5
 				if self.c == "=": # state 6
 				   self.__state = 1
-				   return Token(Tag_type.OP_EQ, "==", self.row, self.column)					
+				   return Token(Tag_type.OP_EQ, "==", self.row, self.column)
 				else:
 					self.__state = 1 # state 7
 					self.file_pointer()
 					self.column = self.column - 1
-					return Token(Tag_type.OP_ASS, "=", self.row, self.column)			
+					return Token(Tag_type.OP_ASS, "=", self.row, self.column)
 
-			
+
 			elif self.__state == 8: # CASE 8
 				if self.c == "=":
 					self.__state = 1 # state 9
-					return Token(Tag_type.OP_GE, ">=", self.row, self.column)	
+					return Token(Tag_type.OP_GE, ">=", self.row, self.column)
 				else:
 					self.__state = 1 # state 10
 					self.file_pointer()
 					self.column = self.column - 1
-					return Token(Tag_type.OP_GT, ">", self.row, self.column)					
+					return Token(Tag_type.OP_GT, ">", self.row, self.column)
 
 			elif self.__state == 11: # CASE 11
 				if self.c == "=":
@@ -226,7 +225,7 @@ class Lexer(object):
 						if self.c == "\t":
 							self.column = self.column + 3
 
-						self.__state = 11		
+						self.__state = 11
 						self.panic_mode(self.row, self.column, "Invalid Character, expected =")
 
 			elif self.__state == 16:
@@ -240,9 +239,9 @@ class Lexer(object):
 					self.__state = 1
 					self.file_pointer()
 					return Token(Tag_type.OP_DIV, "/", self.row, self.column)
-			
 
-			elif self.__state == 17:				
+
+			elif self.__state == 17:
 				if self.c != "*":
 					self.__state = 17 # state 18
 					if self.c == "\n":
@@ -252,9 +251,9 @@ class Lexer(object):
 						self.column = self.column + 3
 					if self.EOF == -1:
   						self.panic_mode(self.row, self.column,"Invalid Character expected */""")
-  						return None					
-				else:	
-					self.__state = 19					
+  						return None
+				else:
+					self.__state = 19
 
 			elif self.__state == 19:
 				if self.c == "/":
@@ -264,7 +263,7 @@ class Lexer(object):
 					if self.EOF == -1:
   						self.panic_mode(self.row, self.column,"Invalid Character expected */""")
   						return None
-  					else:					
+  					else:
 						self.__state = 17
 
 			elif self.__state == 21:
@@ -277,18 +276,18 @@ class Lexer(object):
 						pass
 					else:
 						self.__state = 21
-					
+
 
   			elif self.__state == 31: # CASE 31
   				if self.c.isdigit():
   					self.__list_lexema.append(self.c)
   					self.__state = 31
   				else:
-  					if self.c == ".":  					
+  					if self.c == ".":
   						self.__list_lexema.append(self.c)
   						self.__state = 32 # state 32
   					else:
-	  					self.__state = 1	
+	  					self.__state = 1
 	  					self.file_pointer()
 	  					self.column = self.column - 1
 	  					return Token(Tag_type.CON_NUM, ''.join(map(str, self.__list_lexema)), self.row, self.column)
@@ -309,28 +308,28 @@ class Lexer(object):
   							self.column = self.column + 3
   						self.panic_mode(self.row, self.column, "Error encountered. Expected an integer ")
   						self.__state = 32
-		
+
 
   			elif self.__state == 35: # Case 35
-  				if self.c.isalpha() or self.c.isdigit(): 
+  				if self.c.isalpha() or self.c.isdigit():
   					self.__list_lexema.append(self.c)
   				else:
   					self.__state = 1 # state 36
-  					token =  self.__TS.get_token(''.join(map(str, self.__list_lexema))) # Pesquisa na Tabela de Simbolo. 
+  					token =  self.__TS.get_token(''.join(map(str, self.__list_lexema))) # Pesquisa na Tabela de Simbolo.
   					self.column = self.column - 1
   					self.file_pointer()
   					if token == None:
   						# if not found in the symbol table, insert.
   						token = Token(Tag_type.ID, ''.join(map(str, self.__list_lexema)), self.row, self.column)
-  
+
 	  					self.__TS.put_symbol_table(token) # insert in the symbol table.
 	  					if self.EOF == -1:
 	  						pass
 	  					else:
 	  						#self.file_pointer()
 	  						pass
-	  					#return token # returns a new token object.  				
-	  				
+	  					#return token # returns a new token object.
+
   					return token
 
   			elif self.__state == 37:
@@ -340,10 +339,10 @@ class Lexer(object):
   				if self.c == " " or self.c.isalpha() or self.c.isdigit() :
   					self.__list_lexema.append(self.c)
   					self.__state = 37
- 
- 				elif self.c == "\n": 
+
+ 				elif self.c == "\n":
  					self.row = self.row + 1
- 					self.column = 1				
+ 					self.column = 1
  					self.__state = 37
  					self.panic_mode(self.row, self.column,"Can not have line break :")
 
@@ -359,12 +358,12 @@ class Lexer(object):
   					self.__state = 1
   					self.panic_mode(self.row, self.column,"Invalid Character expected \"")
   					return None
-  					
+
 
   			elif self.__state == 40:
 
   				if self.c.isdigit() or self.c.isalpha() and self.c != "'":
-  					self.__list_lexema.append(self.c) 
+  					self.__list_lexema.append(self.c)
   					self.__state = 40
   				else:
 
@@ -372,8 +371,8 @@ class Lexer(object):
   						self.__state = 1
   						if len(self.__list_lexema) > 1:
   							self.panic_mode(self.row, self.column,"literal must contain only one character")
-  							self.__list_lexema = [] 
-  							#print self.c							
+  							self.__list_lexema = []
+  							#print self.c
 						else:
   							return Token(Tag_type.CON_CHAR, ''.join(map(str, self.__list_lexema)) , self.row, self.column)
   					if self.EOF == -1:
@@ -384,12 +383,12 @@ class Lexer(object):
 def main():
 	"""docstring for Token"""
 	ts = TabelaSimbolo()
-	
+
 	lexer = Lexer('program.pasc', ts)
 	var = True
-	while var:		
+	while var:
 		token = lexer.nex_token()
-		if (token == None) or (token.getLexema() == "EOF"):			
+		if (token == None) or (token.getLexema() == "EOF"):
 			lexer.close_file()
 			var = False
 		else:
